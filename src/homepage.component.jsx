e548e757c719b331db32d4b18244314e990773f9
Kalra <kchirag@amazon.com>
2020-02-11 21:29:37 +0530

    first-commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const homePage = () => {
+  <div className="homepage">
+    <div className="directory-meny">
+      <div className="menu-item"></div>
+    </div>
+  </div>;
+};
