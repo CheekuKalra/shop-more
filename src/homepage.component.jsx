e10a6fe1b554eb9b18b5ac20f22fa97f091d1738
Kalra <kchirag@amazon.com>
2020-02-11 22:59:15 +0530

    adding components

--- src/homepage.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const homePage = () => {
-  <div className="homepage">
-    <div className="directory-meny">
-      <div className="menu-item"></div>
-    </div>
-  </div>;
-};
